3de688b6fbbe5379050b80d8d0f1e08f19bef8bb
Erin Brown <emberbrown@gmail.com>
2014-07-22 14:53:08 -0400

    wrote a failing feature test

--- spec/features/answers_spec.rb
+++ spec/features/answers_spec.rb
@@ -6,7 +6,17 @@ feature 'List all answers ' do
     visit answers_path
     expect(page).to have_content('Response')
     expect(page).to have_content('Responder')
+  end
 
+  scenario 'allows user to create a new answer' do
+    answer = build(:answer)
+    visit new_answer_path
+    fill_in 'Responder', with: answer.responder
+    fill_in 'Response', with: answer.response
+
+    click_button 'Submit'
+    expect(page).to have_content(answer.responder)
+    expect(page).to have_content(answer.response)
   end
 
 
