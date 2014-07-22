cf69425ecff11e9bfa74e12c7d34f7e1a8814e4d
Erin Brown <emberbrown@gmail.com>
2014-07-22 14:29:56 -0400

    added answers_path and an index method for answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,8 @@
 class AnswersController < ActionController::Base
+  render :html
+
+  def index
+    @answers = Answer.all
+    respond_with @answers
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   resources :questions, only: [:index, :new, :create]
+  resources :answers, only: :index
 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
