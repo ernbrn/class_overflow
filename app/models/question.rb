57822cc4823b9fa37648460707a5dd20c10407de
Erin Brown <emberbrown@gmail.com>
2014-07-22 15:15:02 -0400

    added question_id to answers table

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   validates :title, :content, :inquirer, presence: true
+
+  has_many :answers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722184025) do
+ActiveRecord::Schema.define(version: 20140722191438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140722184025) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "responder"
+    t.integer  "question_id"
   end
 
   create_table "questions", force: true do |t|
